cf5f039c6f10a833b3dbbcde3443b84b7034dcde
dineo-matla <dineomatla@outlook.com>
2025-04-06 21:53:03 +0200

    Added new variables and created music track objects to enhance music player functionality

---
+++ script.js
@@ -0,0 +1,34 @@
+let playlistSongs = document.getElementById("playlist-songs");
+let playButton = document.getElementById("play");
+let pauseButton = document.getElementById("pause");
+let nextButton = document.getElementById("next");
+let previousButton = document.getElementById("previous");
+let shuffleButton = document.getElementById("shuffle");
+
+let allSongs = [
+  {
+    id: 0,
+    title: "Scratching The Surface",
+    artist: "Quincy Larson",
+    duration: "4:25",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",
+  },
+  {
+    id: 1,
+    title: "Can't Stay Down",
+    artist: "Quincy Larson",
+    duration: "4:15",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3",
+  },
+  {
+    id: 2,
+    title: "Still Learning",
+    artist: "Quincy Larson",
+    duration: "3:51",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/still-learning.mp3",
+  },
+];
+
+const audio = new Audio();
+let userData = {};
+
