9efaf2a52ab6cf582f3b340d0eb0f32ae25a5cf0
dineo-matla <dineomatla@outlook.com>
2025-04-08 18:46:12 +0200

    Enhanced music management functionality by introducing a song object to consolidate song list data and added properties to monitor current song and playback time

--- script.js
+++ script.js
@@ -8,27 +8,58 @@ let shuffleButton = document.getElementById("shuffle");
 let allSongs = [
   {
     id: 0,
-    title: "Scratching The Surface",
-    artist: "Quincy Larson",
-    duration: "4:25",
-    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",
+    title: "Hey!",
+    artist: "Benjamin Tissot",
+    duration: "2:52",
+    src: "https://www.bensound.com/royalty-free-music/track/hey-happy-cheerful",
   },
   {
     id: 1,
-    title: "Can't Stay Down",
-    artist: "Quincy Larson",
-    duration: "4:15",
-    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3",
+    title: "By My Side",
+    artist: "Marcus P",
+    duration: "2:21",
+    src: "https://www.bensound.com/royalty-free-music/track/by-my-side-joyful-party",
   },
   {
     id: 2,
-    title: "Still Learning",
-    artist: "Quincy Larson",
-    duration: "3:51",
-    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/still-learning.mp3",
+    title: "Unbreakable Resolve",
+    artist: "Lunar Years",
+    duration: "2:04",
+    src: "https://www.bensound.com/royalty-free-music/track/unbreakable-resolve-hopeful-epic",
+  },
+  {
+    id: 3,
+    title: "On Repeat",
+    artist: "Marcus P",
+    duration: "2:16",
+    src: "https://www.bensound.com/royalty-free-music/track/on-repeat-shiny-uplifting",
+  },
+  {
+    id: 4,
+    title: "Love",
+    artist: "Benjamin Tissot",
+    duration: "5:35",
+    src: "https://www.bensound.com/royalty-free-music/track/love",
+  },
+  {
+    id: 5,
+    title: "Memories",
+    artist: "Benjamin Tissot",
+    duration: "3:50",
+    src: "https://www.bensound.com/royalty-free-music/track/memories",
+  },
+  {
+    id: 6,
+    title: "A Day To Remember",
+    artist: "Benjamin Tissot",
+    duration: "3:15",
+    src: "https://www.bensound.com/royalty-free-music/track/a-day-to-remember-wedding-music",
   },
 ];
 
 const audio = new Audio();
-let userData = {};
-
+let userData = {
+  songs: [...allSongs],
+  currentSong: null,
+  songCurrentTime: 0,
+};
