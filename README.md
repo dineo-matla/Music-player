2c787d283fc8a2e8c213a62d67c5560a3027fd75
dineo-matla <dineomatla@outlook.com>
2025-04-09 21:35:18 +0200

    Added new audio file for track organization, refactored src links to correct file paths, and updated README with attribution and copyright information

---
+++ README.md
@@ -0,0 +1,29 @@
+# Attributions
+
+Royalty Free Music: https://www.bensound.com
+Artist: Marcus P.
+License code: 5AE6D15TTT9OZDQ6
+
+Music: bensound.com
+Artist: Marcus P.
+License code: BRHUULFYCWXR1NNZ
+
+Music I Use: Bensound.com/free-music-for-videos
+Artist: Lunar Years
+License code: DINY3FBYYHKF7A63
+
+Music by Bensound.com/royalty-free-music
+Artist: Benjamin Tissot
+License code: 7ZFWNI9SKUIGIQVK
+
+Music: Bensound
+Artist: Benjamin Tissot
+License code: 4AFL7FOMEPEUQBDR
+
+Music by Bensound.com/free-music-for-videos
+Artist: Benjamin Tissot
+License code: OUO4BYQPWPX1NBD2
+
+Music: bensound.com
+Artist: Benjamin Tissot
+License code: XDNXSKROBQTDGZBI
--- script.js
+++ script.js
@@ -11,49 +11,49 @@ let allSongs = [
     title: "Hey!",
     artist: "Benjamin Tissot",
     duration: "2:52",
-    src: "https://www.bensound.com/royalty-free-music/track/hey-happy-cheerful",
+    src: "audio/hey.mp3",
   },
   {
     id: 1,
     title: "By My Side",
     artist: "Marcus P",
     duration: "2:21",
-    src: "https://www.bensound.com/royalty-free-music/track/by-my-side-joyful-party",
+    src: "audio/bymyside.mp3",
   },
   {
     id: 2,
     title: "Unbreakable Resolve",
     artist: "Lunar Years",
     duration: "2:04",
-    src: "https://www.bensound.com/royalty-free-music/track/unbreakable-resolve-hopeful-epic",
+    src: "audio/unbreakableresolve.mp3",
   },
   {
     id: 3,
     title: "On Repeat",
     artist: "Marcus P",
     duration: "2:16",
-    src: "https://www.bensound.com/royalty-free-music/track/on-repeat-shiny-uplifting",
+    src: "audio/onrepeat.mp3",
   },
   {
     id: 4,
     title: "Love",
     artist: "Benjamin Tissot",
     duration: "5:35",
-    src: "https://www.bensound.com/royalty-free-music/track/love",
+    src: "audio/love.mp3",
   },
   {
     id: 5,
     title: "Memories",
     artist: "Benjamin Tissot",
     duration: "3:50",
-    src: "https://www.bensound.com/royalty-free-music/track/memories",
+    src: "audio/memories.mp3",
   },
   {
     id: 6,
     title: "A Day To Remember",
     artist: "Benjamin Tissot",
     duration: "3:15",
-    src: "https://www.bensound.com/royalty-free-music/track/a-day-to-remember-wedding-music",
+    src: "audio/adaytoremember.mp3",
   },
 ];
 
@@ -68,14 +68,14 @@ const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
       return `<li id="song-${song.id}" class="playlist-song"></li>
-
-<button onclick="playSong(${song.id})" class = "playlist-song-info"><span class ="playlist-song-title">${song.title}
+<button onclick= "playSong(${song.id})" class = "playlist-song-info"><span class ="playlist-song-title">${song.title}
 </span><span class = "playlist-song-artist">${song.artist}</span>
 <span class="playlist-song-duration">${song.duration}</span>
 </button>
 <button class = "playlist-song-delete" aria-label = "Delete ${song.title}"><svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg></button>`;
     })
     .join("");
+
   playlistSongs.innerHTML = songsHTML;
 };
 playButton.addEventListener("click", () => {
