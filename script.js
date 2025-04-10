8e90a2a9876de0618da785905c0fb7acf87af6dc
dineo-matla <dineomatla@outlook.com>
2025-04-10 19:50:46 +0200

    Added a shuffle song functionality

--- script.js
+++ script.js
@@ -109,6 +109,16 @@ const playPreviousSong = () => {
   }
 };
 
+const shuffle = () => {
+  userData?.songs.sort(() => Math.random() - 0.5);
+  userData.currentSong = null;
+  userData.songCurrentTime = 0;
+  renderSongs(userData?.songs);
+  pauseSong();
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
+};
+
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
   const songArtist = document.getElementById("player-song-artist");
@@ -168,6 +178,7 @@ playButton.addEventListener("click", () => {
 pauseButton.addEventListener("click", pauseSong);
 nextButton.addEventListener("click", playNextSong);
 previousButton.addEventListener("click", playPreviousSong);
+shuffleButton.addEventListener("click", shuffle);
 
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {
