f69f7b97cb620fe898f494d2c5bf34bc64266e22
dineo-matla <dineomatla@outlook.com>
2025-04-10 21:14:08 +0200

    Implemented functionality to reset the playlist should users make changes

--- script.js
+++ script.js
@@ -167,6 +167,22 @@ const renderSongs = (array) => {
     .join("");
 
   playlistSongs.innerHTML = songsHTML;
+
+  if (userData?.songs.length === 0) {
+    const resetButton = document.createElement("button");
+    const resetText = document.createTextNode("Reset Playlist");
+
+    resetButton.id = "reset";
+    resetButton.ariaLabel = "Reset playlist";
+    resetButton.appendChild(resetText);
+    playlistSongs.appendChild(resetButton);
+    resetButton.addEventListener("click", () => {
+      userData.songs = [...allSongs];
+      renderSongs(sortSongs());
+      setPlayButtonAccessibleText();
+      resetButton.remove();
+    });
+  }
 };
 
 const setPlayButtonAccessibleText = () => {
